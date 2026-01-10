0fbdb240c782ca1094309538f82d2b26b229a4a8
VagnerJr <vagner.junior@teddydigital.io>
2026-01-10 18:45:26 -0300

    build/chore: jest

---
+++ jest.config.mjs
@@ -0,0 +1,28 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  moduleFileExtensions: [
+    "js",
+    "mjs",
+    "cjs",
+    "jsx",
+    "ts",
+    "mts",
+    "cts",
+    "tsx",
+    "json",
+    "node",
+  ],
+  testEnvironment: "node",
+  transform: {},
+};
+
+export default config;
